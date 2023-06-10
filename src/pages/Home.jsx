9ccfd6c9be71a4e40ff2391d53a2036cf5b28473
juliozm20 <juliozm20@outlook.es>
2023-06-09 22:27:49 -0600

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import ListPost from "../components/ListPost";
+import ListCategories from "../components/ListCategories";
+
+const Home = () => {
+  return (
+    <main>
+      <div className="container">
+        <h2 className="title-page">Pet noticias</h2>
+      </div>
+      <ListCategories />
+      <ListPost url={"/posts"} />
+    </main>
+  );
+};
+
+export default Home;
